111c6b75aceddab835f69881a4039fa31a667829
pavel <ufamail.com2@gmail.com>
2022-05-29 22:49:41 +0500

    first commit

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "", //ftp адрес
+    user: "",// имя пользователя
+    password: "", //пароль
+    paralel: 5,//кол-во одновременных потоков
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# my Gulp assembly
+
+before use install all pachages(npm i/npm install)
+
+for run write: npm run dev
+for archive project: npm run zip
